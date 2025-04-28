99d6c3ece881a7e2d4718442106f8d29352c82ab
NiKuliCat <113290925+NiKuliCat@users.noreply.github.com>
2025-04-28 11:25:29 +0800

    修改premake

--- premake5.lua
+++ premake5.lua
@@ -34,7 +34,6 @@ project "GLFW"
 
 	filter "system:windows"
         systemversion "latest"
-        staticruntime "on"
 
         files
         {
@@ -58,36 +57,15 @@ project "GLFW"
             "_CRT_SECURE_NO_WARNINGS"
         }
 
-    filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		staticruntime "on"
-
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
+        
     filter "configurations:Debug"
         runtime "Debug"
-        symbols "on"
+        symbols "On"
 
     filter "configurations:Release"
         runtime "Release"
-        optimize "on"
+        optimize "On"
+
+
+
         
